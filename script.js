2de4a1f6a55189990366b716d0ae75a47932d79a
jesslynhh <jesslyn.hok@gmail.com>
2024-03-30 21:53:54 +0700

    login v1.3

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ $(document).ready(function(){
 		setTimeout(autoplay, 4500);
 	}
   });
+
+const togglePassword = document
+.querySelector('#toggle-password');
+const password = document.querySelector('#password');
+togglePassword.addEventListener('click', () => {
+  // Toggle the type attribute using
+  // getAttribure() method
+const type = password
+	.getAttribute('type') === 'password' ?
+	'text' : 'password';
+password.setAttribute('type', type);
+  // Toggle the eye and bi-eye icon
+this.classList.toggle('bi-eye');
+});
+
+function password_show_hide() {
+	var x = document.getElementById("password");
+	var show_eye = document.getElementById("show_eye");
+	var hide_eye = document.getElementById("hide_eye");
+	hide_eye.classList.remove("d-none");
+	if (x.type === "password") {
+	  x.type = "text";
+	  show_eye.style.display = "none";
+	  hide_eye.style.display = "block";
+	} else {
+	  x.type = "password";
+	  show_eye.style.display = "block";
+	  hide_eye.style.display = "none";
+	}
+  }
