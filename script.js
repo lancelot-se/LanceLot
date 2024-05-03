d62c6888c7158398fcb64f9bed9c6a4c4f38181e
ichelmichelll <michelle.winata31@gmail.com>
2024-05-03 14:28:48 +0700

    new updates

--- script.js
+++ script.js
@@ -38,3 +38,19 @@ function password_show_hide() {
 	  hide_eye.style.display = "none";
 	}
   }
+
+  function password_show_hide2() {
+	var x = document.getElementById("password");
+	var show_eye = document.getElementById("show_eye2");
+	var hide_eye = document.getElementById("hide_eye2");
+	hide_eye.classList.remove("d-none");
+	if (x.type === "password") {
+	  x.type = "text";
+	  show_eye.style.display = "none";
+	  hide_eye.style.display = "block";
+	} else {
+	  x.type = "password";
+	  show_eye.style.display = "block";
+	  hide_eye.style.display = "none";
+	}
+  }
